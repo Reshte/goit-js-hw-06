53d4015ff8d8410553dc269b8614f7a58ae41c39
Reshte <107108359+Reshte@users.noreply.github.com>
2022-10-19 18:05:55 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+//перевірити ще раз
+const listEl = document.querySelector("#ingredients");
+
+const makeListElement = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = ingredient;
+    itemEl.classList.add("item");
+    return itemEl;
+  });
+};
+
+const makeList = makeListElement(ingredients);
+
+listEl.append(...makeList);
+console.log(listEl);
