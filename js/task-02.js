6591223663dfa4fac6d090a0b9481b9db07e8ff1
Reshte <107108359+Reshte@users.noreply.github.com>
2022-10-20 10:07:53 +0300

    task - 03

--- js/task-02.js
+++ js/task-02.js
@@ -7,19 +7,40 @@ const ingredients = [
   "Condiments",
 ];
 
-//перевірити ще раз
+//Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+// Первый вариант решения задачи.
+
+// const listEl = document.querySelector("#ingredients");
+
+// const makeListElement = (ingredients) => {
+//   return ingredients.map((ingredient) => {
+//     const itemEl = document.createElement("li");
+//     itemEl.textContent = ingredient;
+//     itemEl.classList.add("item");
+//     return itemEl;
+//   });
+// };
+
+// const makeList = makeListElement(ingredients);
+
+// listEl.append(...makeList);
+// console.log(listEl);
+
+//Второй вариант решения задачи.
+
 const listEl = document.querySelector("#ingredients");
+const listArr = [];
 
-const makeListElement = (ingredients) => {
-  return ingredients.map((ingredient) => {
-    const itemEl = document.createElement("li");
-    itemEl.textContent = ingredient;
-    itemEl.classList.add("item");
-    return itemEl;
-  });
-};
-
-const makeList = makeListElement(ingredients);
-
-listEl.append(...makeList);
-console.log(listEl);
+ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  listArr.push(itemEl);
+});
+listEl.append(...listArr);
