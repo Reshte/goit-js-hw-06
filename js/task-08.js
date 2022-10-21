f90a197a2adf3f2f0ffc68c942e1528a734a37f4
Reshte <107108359+Reshte@users.noreply.github.com>
2022-10-21 14:51:11 +0300

    task -9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+// Напиши скрипт управления формой логина.
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
+const formRef = document.querySelector(".login-form");
+const emailInputRef = document.querySelector(".email-input");
+const passwordInputRef = document.querySelector(".password-input");
+const submitBtnRef = document.querySelector(".submit-btn");
+
+formRef.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { login, password },
+  } = event.currentTarget;
+  console.log(login.value);
+  if (login.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+// Напиши скрипт, который изменяет цвета фона элемента < body > через инлайн стиль
+//  при клике на button.change - color и выводит значение цвета в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanRef = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", (event) => {
+  console.log(getRandomHexColor());
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
+});
