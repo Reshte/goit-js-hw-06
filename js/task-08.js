31ca38c340f71d57b97e6cb3a870ddb43ec13d7e
Reshte <107108359+Reshte@users.noreply.github.com>
2022-10-23 12:16:09 +0300

    task - 08

--- js/task-08.js
+++ js/task-08.js
@@ -2,25 +2,32 @@
 // Обработка отправки формы form.login-form должна быть по событию submit.
 // При отправке формы страница не должна перезагружаться.
 // Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
-// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства,
+//  а значение поля - значением свойства.Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
 const formRef = document.querySelector(".login-form");
-const emailInputRef = document.querySelector(".email-input");
-const passwordInputRef = document.querySelector(".password-input");
 const submitBtnRef = document.querySelector(".submit-btn");
 
 formRef.addEventListener("submit", handleSubmit);
+const formData = {};
+
 function handleSubmit(event) {
   event.preventDefault();
+
   const {
-    elements: { login, password },
+    elements: { email, password },
   } = event.currentTarget;
-  console.log(login.value);
-  if (login.value === "" || password.value === "") {
-    return console.log("Please fill in all the fields!");
+
+  if (email.value === "" || password.value === "") {
+    return alert(
+      "Пожалуйста, введите ваши данные. Необходимо заполнить все поля"
+    );
   }
 
-  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  formData.email = email.value;
+  formData.password = password.value;
+
+  console.log(formData);
   event.currentTarget.reset();
 }
